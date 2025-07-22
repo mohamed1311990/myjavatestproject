83f57acf52d3ba20198f8464311443ad49b3c53f
mhoamed1311990 <mohamed.salem.say@gmail.com>
2025-07-22 12:54:01 +0200

    testing keep alive

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -1,6 +1,8 @@
+package com.example;
+
 public class HelloApp {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello, World!");
-        Thread.sleep(Long.MAX_VALUE); // Keep it alive forever
+        System.out.println("Hello from Java inside Docker!");
+        Thread.sleep(Long.MAX_VALUE); // Keep the app running indefinitely
     }
 }
