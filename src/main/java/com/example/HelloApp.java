66d4beddb5162b5f45561f7d7c9b9f8d3845237d
mohamed1311990 <mohamed.salem.say@gmail.com>
2025-07-22 12:20:05 +0200

    change class name

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -1,4 +1,4 @@
-public class Main {
+public class HelloApp {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, World!");
         Thread.sleep(Long.MAX_VALUE); // Keep it alive forever
