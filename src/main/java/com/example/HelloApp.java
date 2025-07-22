d1d95afd97babc626dbce709ad16321a13701915
mohamed1311990 <mohamed.salem.say@gmail.com>
2025-07-22 14:00:51 +0200

    Update HelloApp.java

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloApp {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello from Java inside Docker!");
+        System.out.println("Hello from Java inside Docker V2!");
         Thread.sleep(Long.MAX_VALUE); // Keep the app running indefinitely
     }
 }
